fe5c7bb3508568eb88320e4e719fb7c01cda41da
agushyn <96276756+agushyn@users.noreply.github.com>
2026-08-18 13:33:55 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    outDir: 'dist',
-  }
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.jsx'], // Pastikan file app.jsx atau app.tsx benar ada
+            refresh: true,
+        }),
+        react(),
+        tailwindcss(),
+    ],
 });
